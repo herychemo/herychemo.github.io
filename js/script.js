9f1cad057a0c4484616d6208749327511f3e19be
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-01-20 22:07:00 -0600

    added Parallax Effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
+
+var p_windows = 0;
+
 var main = function(){
 	console.log("Hi Friends");
+
+	var image_path_base = "./images/bg$.jpg";
+	
+	p_windows = $(".parallax-window");	
+	
+	$.each(p_windows, function(index, item) {
+
+		$(item).parallax({
+			imageSrc: image_path_base.replace( '$', index),
+			naturalWidth : 3840,
+			naturalHeight : 2160,
+			bleed : 10
+		});
+	});
+
+	$(window).on("resize", function(e){
+		on_resize();
+	});
+	on_resize();
 }
 $(main);
+
+function on_resize(){
+	p_windows.css('minHeight', $(window).height() );
+}
