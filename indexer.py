74dd665c935837252dcb465bb8cf17e4f863b7b4
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-01-29 13:02:27 -0600

    added indexer
    implementation is pending

---
+++ indexer.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+mypath = "./" 
+
+my_indexes = {} 
+
+def list_all(mypath ) :
+    d = [] 
+    f = []
+    for (dirpath, dirnames, filenames) in os.walk(mypath):
+        f.extend(filenames)
+        d.extend(dirnames )
+        return f, d
+
+def get_obj(cur_path) :
+    out = {} 
+    files, dirs = list_all(cur_path)
+    out['files'] = files
+    temp = {} 
+    for dir in dirs:
+        temp[dir] = get_obj( os.path.join(cur_path, dir) ) 
+    out['dirs'] = temp
+    return out
+    
+
+my_indexes = get_obj(mypath ) 
+
+json_data = json.dumps(my_indexes, indent=4)
+print(json_data )
